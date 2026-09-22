88dbeb49d8287404ced91d59cbe75ca9cdb5ee80
Adrian Lipa <adrianlipa90@gmail.com>
2026-09-22 05:53:40 +0100

    feat: expose exotic biology framework

---
+++ origins/exobiology/__init__.py
@@ -0,0 +1,25 @@
+from .profiles import (
+    AMMONIA_CANDIDATE,
+    HYDROCARBON_CANDIDATE,
+    LIFE_RELATIONAL_INVARIANTS,
+    WATER_REFERENCE,
+    BiochemistryProfile,
+    EpistemicStatus,
+    RuntimeStatus,
+    WorldEnvironment,
+    available_biochemistry_profiles,
+    get_biochemistry_profile,
+)
+
+__all__ = [
+    "AMMONIA_CANDIDATE",
+    "HYDROCARBON_CANDIDATE",
+    "LIFE_RELATIONAL_INVARIANTS",
+    "WATER_REFERENCE",
+    "BiochemistryProfile",
+    "EpistemicStatus",
+    "RuntimeStatus",
+    "WorldEnvironment",
+    "available_biochemistry_profiles",
+    "get_biochemistry_profile",
+]
